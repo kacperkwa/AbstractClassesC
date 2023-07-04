d1a5a7eaad835dad78b620a0a372e7ba2c266fbc
kacperkwa <kacperkwartaxz@gmail.com>
2023-07-04 18:04:21 +0200

    Add project files.

---
+++ AbstractClassesC/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AbstractClassesC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Shape[] shapes =
+            {
+                new Sphere(4), new Cube(4)
+            };
+
+            foreach (Shape shape in shapes)
+            {
+                shape.GetInfo();
+                Console.WriteLine($"{shape.Name} has a volume of {shape.Volume()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
