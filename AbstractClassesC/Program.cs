03bf6cd9ff81f920cb38094d3397097a7cd74c2f
kacperkwa <kacperkwartaxz@gmail.com>
2023-07-04 18:47:18 +0200

    Added new functionality of abstract classes

--- AbstractClassesC/Program.cs
+++ AbstractClassesC/Program.cs
@@ -14,6 +14,21 @@
             {
                 shape.GetInfo();
                 Console.WriteLine($"{shape.Name} has a volume of {shape.Volume()}");
+
+                Cube iceCube = shape as Cube;
+                if (iceCube == null)
+                {
+                    Console.WriteLine("This Shape is no cube!");
+                }
+
+                if (shape is Cube)
+                {
+                    Console.WriteLine("This Shape is a cube!");
+                }
+
+                object cube1 = new Cube(7);
+                Cube cube2 = (Cube)cube1;
+                Console.WriteLine($"{cube2.Name} has a Voulume of {cube2.Volume()}");
             }
             Console.ReadLine();
         }
